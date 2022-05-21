37c890f092941d305c46001d02481c2870f5d835
AnandaIlyasa <anandailyasa23244@student.uns.ac.id>
2022-05-21 19:58:04 +0800

    adding model prediction to api

--- api/app/app.py
+++ api/app/app.py
@@ -1,9 +1,13 @@
 from PIL import Image
 import numpy as np
-from flask import Flask, request
+from flask import Flask, jsonify, request
+from tensorflow.keras.models import load_model
 
 app = Flask(__name__)
- 
+
+food_classifier = load_model('../model_25_classes',compile = False)
+food_list = ["apple_pie","bakso","bibimbap","bread_pudding","cheesecake","chicken_curry","chicken_wings","chocolate_cake","french_fries","gado","garlic_bread","gnocchi","gudeg","hamburger","omelette","pizza","rendang","samosa","sate","shrimp_and_grits","strawberry_shortcake","tacos","tiramisu","tuna_tartare","waffles"]
+
 @app.route('/')
 def hello_world():
     return "<h1>Welcome</h1>"
@@ -12,7 +16,22 @@ def hello_world():
 def upload():
     image = request.files['gambar']
     img = Image.open(image)
-    img.resize((300,300))
+
     img_array = np.array(img)
+    img_array = np.expand_dims(img_array, axis=0)
+
+    pred = food_classifier.predict(img_array)
+    food_list.sort()
+    top_n = 3
+    indices = np.argpartition(pred, -top_n)[-top_n:]
+    indices = np.squeeze(indices)
+    indices = np.flip(indices)
+
+    response = {
+        food_list[indices[0]]: pred[0][indices[0]],
+        food_list[indices[1]]: pred[0][indices[1]],
+        food_list[indices[2]]: pred[0][indices[2]],
+    }
+
     img.close()
-    return "<h1>Upload succesful</h1>"
+    return jsonify(response)
